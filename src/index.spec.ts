07720bd96ad55ed33d47ff6b5ec0bbdcce3bdac3
Aaron Walker <aaron.walker@financial-cloud.com>
2020-03-05 16:01:45 +0000

    refactor: interfaces to file

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,140 +1,142 @@
-import { IChecks, Validator, validations } from './index';
+import { Validator, validations } from './index';
+
+import { IChecks } from './interfaces';
 
 test('Should pass with simple input', (done) => {
-    const input = {
-        name: 'vorticalbox',
-    };
-    const checks: IChecks[] = [
-        {
-            required: true,
-            message: 'name must be a string',
-            key: 'name',
-            checks: [validations.isString],
-        },
-    ];
-    const v = new Validator(checks, input);
-    v.validate();
-    expect(v.valid).toEqual(true);
-    done();
+  const input = {
+    name: 'vorticalbox',
+  };
+  const checks: IChecks[] = [
+    {
+      required: true,
+      message: 'name must be a string',
+      key: 'name',
+      checks: [validations.isString],
+    },
+  ];
+  const v = new Validator(checks, input);
+  v.validate();
+  expect(v.valid).toEqual(true);
+  done();
 });
 
 test('Should fail invalid type', (done) => {
-    const input = {
-        name: 1234,
-    };
-    const checks: IChecks[] = [
-        {
-            required: true,
-            message: 'name must be a string',
-            key: 'name',
-            checks: [validations.isString],
-        },
-    ];
-    const v = new Validator(checks, input);
-    v.validate();
-    expect(v.valid).toEqual(false);
-    const [error] = v.errors();
-    expect(error.message).toEqual('name must be a string');
-    done();
+  const input = {
+    name: 1234,
+  };
+  const checks: IChecks[] = [
+    {
+      required: true,
+      message: 'name must be a string',
+      key: 'name',
+      checks: [validations.isString],
+    },
+  ];
+  const v = new Validator(checks, input);
+  v.validate();
+  expect(v.valid).toEqual(false);
+  const [error] = v.errors();
+  expect(error.message).toEqual('name must be a string');
+  done();
 });
 
 test('Should fail with no value', (done) => {
-    const input = {};
-    const checks: IChecks[] = [
-        {
-            required: true,
-            message: 'name must be a string',
-            key: 'name',
-            checks: [validations.isString],
-        },
-    ];
-    const v = new Validator(checks, input);
-    v.validate();
-    expect(v.valid).toEqual(false);
-    const [error] = v.errors();
-    expect(error.message).toEqual('name is required');
-    done();
+  const input = {};
+  const checks: IChecks[] = [
+    {
+      required: true,
+      message: 'name must be a string',
+      key: 'name',
+      checks: [validations.isString],
+    },
+  ];
+  const v = new Validator(checks, input);
+  v.validate();
+  expect(v.valid).toEqual(false);
+  const [error] = v.errors();
+  expect(error.message).toEqual('name is required');
+  done();
 });
 
 test('Should fail invalid input type', (done) => {
-    const input: any = [];
-    const checks: IChecks[] = [
-        {
-            required: true,
-            message: 'name must be a string',
-            key: 'name',
-            checks: [validations.isString],
-        },
-    ];
-    const v = new Validator(checks, input);
-    v.validate();
-    expect(v.valid).toEqual(false);
-    const [error] = v.errors();
-    expect(error.message).toEqual('input must be an object');
-    done();
+  const input: any = [];
+  const checks: IChecks[] = [
+    {
+      required: true,
+      message: 'name must be a string',
+      key: 'name',
+      checks: [validations.isString],
+    },
+  ];
+  const v = new Validator(checks, input);
+  v.validate();
+  expect(v.valid).toEqual(false);
+  const [error] = v.errors();
+  expect(error.message).toEqual('input must be an object');
+  done();
 });
 
 test('Should pass with missing key', (done) => {
-    const input = {
-        username: 'vorticalbox',
-    };
-    const checks: IChecks[] = [
-        {
-            required: true,
-            message: 'username must be a string',
-            key: 'username',
-            checks: [validations.isString],
-        },
-        {
-            required: false,
-            message: 'email must be a string',
-            key: 'email',
-            checks: [validations.isString],
-        },
-    ];
-    const v = new Validator(checks, input);
-    v.validate();
-    expect(v.valid).toEqual(true);
-    done();
+  const input = {
+    username: 'vorticalbox',
+  };
+  const checks: IChecks[] = [
+    {
+      required: true,
+      message: 'username must be a string',
+      key: 'username',
+      checks: [validations.isString],
+    },
+    {
+      required: false,
+      message: 'email must be a string',
+      key: 'email',
+      checks: [validations.isString],
+    },
+  ];
+  const v = new Validator(checks, input);
+  v.validate();
+  expect(v.valid).toEqual(true);
+  done();
 });
 
 test('Custom validation function', (done) => {
-    const input: any = {
-        status: 'broken',
-    };
-    const contains = (value:any): boolean => ['valid', 'invalid'].includes(value);
-    const checks: IChecks[] = [
-        {
-            required: true,
-            message: 'must be valid or invalid',
-            key: 'status',
-            checks: [contains],
-        },
+  const input: any = {
+    status: 'broken',
+  };
+  const contains = (value:any): boolean => ['valid', 'invalid'].includes(value);
+  const checks: IChecks[] = [
+    {
+      required: true,
+      message: 'must be valid or invalid',
+      key: 'status',
+      checks: [contains],
+    },
 
-    ];
-    const v = new Validator(checks, input);
-    v.validate();
-    expect(v.valid).toEqual(false);
-    const [error] = v.errors();
-    expect(error.message).toEqual('must be valid or invalid');
-    expect(error.value).toEqual(input.status);
-    done();
+  ];
+  const v = new Validator(checks, input);
+  v.validate();
+  expect(v.valid).toEqual(false);
+  const [error] = v.errors();
+  expect(error.message).toEqual('must be valid or invalid');
+  expect(error.value).toEqual(input.status);
+  done();
 });
 
 test('Should pass with simple input - thread', (done) => {
-    const input = {
-        name: 'vorticalbox',
-    };
-    const checks: IChecks[] = [
-        {
-            required: true,
-            message: 'name must be a string',
-            key: 'name',
-            checks: [validations.isString],
-        },
-    ];
-    const v = new Validator(checks, input);
-    v.validate();
-    expect(v.valid).toEqual(true);
-    done();
+  const input = {
+    name: 'vorticalbox',
+  };
+  const checks: IChecks[] = [
+    {
+      required: true,
+      message: 'name must be a string',
+      key: 'name',
+      checks: [validations.isString],
+    },
+  ];
+  const v = new Validator(checks, input);
+  v.validate();
+  expect(v.valid).toEqual(true);
+  done();
 });
--- src/index.ts
+++ src/index.ts
@@ -1,59 +1,47 @@
 import * as R from 'ramda';
 import * as RA from 'ramda-adjunct';
 
-interface ICheck {
-    (value: any, args: [any]): boolean;
-}
-export interface IChecks {
-    required: boolean;
-    message: string;
-    key: string;
-    checks: ICheck[]
-}
-
-interface IErrors {
-    message: string;
-    value: any; // value that failed check
-}
+import { ICheck, IChecks, IErrors } from './interfaces';
 
 const get = (path: string, object: any) => R.view(R.lensPath(R.split('.', path)), object);
 export const validations = RA;
 export class Validator {
-    private validations: IChecks[]
-    public _errors: IErrors[] = [];
+    private validations: IChecks[];
+
+    private foundErrors: IErrors[] = [];
+
     public valid: boolean = false;
+
     private input: any;
 
-    constructor(validations: IChecks[], input: any) {
-        this.validations = validations;
-        this.input = input;
+    constructor(passedValidations: IChecks[], input: any) {
+      this.validations = passedValidations;
+      this.input = input;
     }
 
     public validate(): void {
-        if (!RA.isObjLike(this.input) || Array.isArray(this.input)) {
-            this._errors.push({ message: 'input must be an object', value: this.input });
-        } else {
-            this.validations.forEach((validation) => {
-                const required: boolean = validation.required;
-                const message: string = validation.message;
-                const key:string = validation.key;
-                const value = get(key, this.input);
+      if (!RA.isObjLike(this.input) || Array.isArray(this.input)) {
+        this.foundErrors.push({ message: 'input must be an object', value: this.input });
+      } else {
+        this.validations.forEach((validation) => {
+          const { required, message, key } = validation;
+          const value = get(key, this.input);
 
-                if (required && RA.isUndefined(value)) {
-                    this._errors.push({ message: `${key} is required`, value });
-                } else {
-                    const check = R.allPass(validation.checks);
-                    const result: boolean = check(value);
-                    if (RA.isFalse(result) && required) {
-                        this._errors.push({ message, value });
-                    }
-                }
-            });
-        }
-        this.valid = this._errors.length === 0;
+          if (required && RA.isUndefined(value)) {
+            this.foundErrors.push({ message: `${key} is required`, value });
+          } else {
+            const check = R.allPass(validation.checks);
+            const result: boolean = check(value);
+            if (RA.isFalse(result) && required) {
+              this.foundErrors.push({ message, value });
+            }
+          }
+        });
+      }
+      this.valid = this.foundErrors.length === 0;
     }
 
     public errors(): IErrors[] {
-        return this._errors;
+      return this.foundErrors;
     }
 }
